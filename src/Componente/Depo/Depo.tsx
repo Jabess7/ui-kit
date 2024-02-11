70e53d8549af838f425d83104d7e7b7bc2b8c3f8
Jabess7 <jabess7@gmail.com>
2024-02-11 15:35:45 -0300

    banner depo

---
+++ src/Componente/Depo/Depo.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import style from "./Depo.module.css"
+import iconAirbn from "../../assets/icon-airbn.svg"
+import iconHubSport from "../../assets/icon-hubsport.svg"
+import iconStripi from "../../assets/icon-strapi.svg"
+
+
+const Depo = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Depo
--- src/Pages/Depoimentos/Depoimentos.tsx
+++ src/Pages/Depoimentos/Depoimentos.tsx
@@ -1,21 +1,44 @@
 import React from 'react'
 import style from"./Depoimentos.module.css"
 import iconBack from "../../assets/icon-background.svg"
+import iconAirbn from "../../assets/icon-airbn.svg"
+import iconHubSport from "../../assets/icon-hubsport.svg"
+import iconStripi from "../../assets/icon-strapi.svg"
+import iconDepo from "./icon-depo.svg"
+
 
 
 const Depoimentos = () => {
   return (
     <div className={style.principal}>
         <div className={style.container1}>
-        <img src={iconBack} alt=""  />
-        <div className={style.titulo}>
-        
-        <h2>Real Stories from Real Customers</h2>
-        <p>Get inspired by these stories.</p>
-        </div>
+            <img src={iconBack} alt=""  />
             
+            <div className={style.titulo}>        
+                <h2>Real Stories from Real Customers</h2>
+                <p>Get inspired by these stories.</p>
+                
+        <div className={style.depo1}>            
+            <img src={iconHubSport} alt="" className={style.lg1} /><br />
+            <img src={iconDepo} alt="" className={style.icoDepo} />
+            <p>To quickly start my startup <br />
+                landing page design, I was <br />
+                looking for a landing page UI Kit. <br />
+                Landify is one of the best landing <br />
+                page UI kit I have come across. <br />
+                It's so flexible, well organised and <br />
+                easily editable.</p>
+            <div className={style.descri}>
+                <h4>Floyd Miles</h4>
+                <p >Vice President, GoPro</p>
+            </div>
+           
         </div>
-        <div>
+        </div>
+         </div>
+        <div className={style.container2}>
+        <img src={iconAirbn} alt="" />
+        <img src={iconStripi} alt="" />
 
         </div>
     </div>
