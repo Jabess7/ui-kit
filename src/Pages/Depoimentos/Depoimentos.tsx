5ce367e55586ed40de3fa44cdc7f54cb7ebac243
Jabess7 <jabess7@gmail.com>
2024-02-11 20:43:05 -0300

    responsividade

--- src/Pages/Depoimentos/Depoimentos.tsx
+++ src/Pages/Depoimentos/Depoimentos.tsx
@@ -11,34 +11,34 @@ import iconDepo from "./icon-depo.svg"
 const Depoimentos = () => {
   return (
     <div className={style.principal}>
-        <div className={style.container1}>
-            <img src={iconBack} alt=""  />
+        <div>
+                <div className={style.container1}>
+                <img src={iconBack} alt=""  />
             
-            <div className={style.titulo}>        
-                <h2>Real Stories from Real Customers</h2>
-                <p>Get inspired by these stories.</p>
-                
-        <div className={style.depo1}>            
-            <img src={iconHubSport} alt="" className={style.lg1} /><br />
-            <img src={iconDepo} alt="" className={style.icoDepo} />
-            <p>To quickly start my startup <br />
-                landing page design, I was <br />
-                looking for a landing page UI Kit. <br />
-                Landify is one of the best landing <br />
-                page UI kit I have come across. <br />
-                It's so flexible, well organised and <br />
-                easily editable.</p>
-            <div className={style.descri}>
-                <h4>Floyd Miles</h4>
-                <p >Vice President, GoPro</p>
-            </div>
-           
-        </div>
+                    <div className={style.titulo}>
+                    <h2>Real Stories from Real Customers</h2>
+                    <p>Get inspired by these stories.</p>
+                    </div>                
+            <div className={style.depoimento}>
+            <div className={style.depo1}>
+                <img src={iconHubSport} alt="" className={style.lg1} /><br />
+                <img src={iconDepo} alt="" className={style.icoDepo} />
+                <p>To quickly start my startup <br />
+                    landing page design, I was <br />
+                    looking for a landing page UI Kit. <br />
+                    Landify is one of the best landing <br />
+                    page UI kit I have come across. <br />
+                    It's so flexible, well organised and <br />
+                    easily editable.</p>
+                <div className={style.descri}>
+                    <h4>Floyd Miles</h4>
+                    <p >Vice President, GoPro</p>
+                </div>
+                </div>
+                 </div>
         </div>
          </div>
         <div className={style.container2}>
-        
-
             <div className={style.depo2}>            
                 <img src={iconAirbn} alt="" className={style.lg1} /><br />
                 <img src={iconDepo} alt="" className={style.icoDepo} />
